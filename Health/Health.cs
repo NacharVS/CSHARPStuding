9c4899f9e0a8108abf5006e4e65960485cc264b1
r3dinc <r3dinc@yandex.ru>
2020-10-10 14:01:42 +0300

    first commit

---
+++ Health/Health.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace HealthControlling
+{
+    public sealed class Health
+    {
+        public delegate void ChangedDelegate(int health, int value);
+
+        private int _max;
+        private int _value;
+
+        public Health(int max)
+        {
+            Max = max;
+        }
+
+        public int Max
+        {
+            get => _max;
+            set
+            {
+                if (value == _max)
+                    return;
+
+                var oldMax = _max;
+                _max = value;
+                var maxDiff = _max - oldMax;
+
+                if (_value > _max)
+                {
+                    var oldValue = _value;
+                    _value = _max;
+                    var diff = _value - oldValue;
+                    ValueChangedEvent?.Invoke(_value, diff);
+                }
+
+                MaxChangedEvent?.Invoke(_max, maxDiff);
+            }
+        }
+
+        public int Value
+        {
+            get => _value;
+            set
+            {
+                if (value == _value)
+                    return;
+
+                var oldValue = _value;
+                _value = value > Max ? Max : value;
+                if (_value < 0)
+                    _value = 0;
+                var diff = _value - oldValue;
+
+                ValueChangedEvent?.Invoke(_value, diff);
+
+                if (IsDead)
+                    DeathEvent?.Invoke();
+            }
+        }
+
+        public bool IsAlive => _value > 0;
+        public bool IsDead => _value <= 0;
+
+
+        public event ChangedDelegate ValueChangedEvent;
+        public event ChangedDelegate MaxChangedEvent;
+        public event Action DeathEvent;
+
+        public static implicit operator int(Health health) => health.Value;
+
+        public static Health operator +(Health health, int value)
+        {
+            health.ValueAdd(value);
+            return health;
+        }
+
+        public static Health operator -(Health health, int value)
+        {
+            health.ValueRemove(value);
+            return health;
+        }
+    }
+}
---
+++ Health/HealthExtensions.cs
@@ -0,0 +1,53 @@
+﻿namespace HealthControlling
+{
+    public static class HealthExtension
+    {
+        public static (int, int) ValueSet(this Health health, int value)
+        {
+            var current = health.Value;
+            health.Value = value;
+            var diff = current - health.Value;
+            return (health.Value, diff);
+        }
+
+        public static (int, int) ValueAdd(this Health health, int value)
+        {
+            var current = health.Value;
+            health.Value += value;
+            var diff = health.Value - current;
+            return (health.Value, diff);
+        }
+
+        public static (int, int) ValueRemove(this Health health, int value)
+        {
+            var current = health.Value;
+            health.Value -= value;
+            var diff = current - health.Value;
+            return (health.Value, diff);
+        }
+
+        public static (int, int) MaxSet(this Health health, int value)
+        {
+            var current = health.Max;
+            health.Max = value;
+            var diff = health.Max - current;
+            return (health.Max, diff);
+        }
+
+        public static (int, int) MaxAdd(this Health health, int value)
+        {
+            var current = health.Max;
+            health.Max += value;
+            var diff = health.Max - current;
+            return (health.Max, diff);
+        }
+
+        public static (int, int) MaxRemove(this Health health, int value)
+        {
+            var current = health.Max;
+            health.Max -= value;
+            var diff = health.Max - current;
+            return (health.Max, diff);
+        }
+    }
+}
