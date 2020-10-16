9e65d66de3cbc924d5551f4da39d90c7e9ca984e
Kamikotoro <nanano102030@mail.ru>
2020-10-16 23:43:37 +0300

    Add base class Baff Add baff HealthBaff not final version

---
+++ Health/Baff.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HealthControlling
+{
+    //Возможны доработки
+    public abstract class Baff
+    {
+        public delegate void BaffStatusChanged();
+        private string _baffName;
+        private double _activeTime;
+        private int _baffStrength;
+
+        public int BaffStrength 
+        {
+            
+            get => _baffStrength;          
+            set => _baffStrength=value;
+        }
+        public string BaffName
+        { 
+            get => _baffName;
+            set => _baffName = value;
+        }
+        public double ActiveTime
+        {
+            get=>_activeTime;
+            set
+            {
+                _activeTime = value;
+                if(_activeTime<=0)
+                {
+                    Deactivate();
+                }        
+            }
+        }
+
+        public Baff(string name , int strength , double time) 
+        {
+            _baffName = name;
+            _baffStrength = strength;
+            _activeTime = time;
+            Activate();
+        }
+
+        //Активация баффа
+        public virtual void Activate() 
+        {
+            StartTimer(_activeTime);
+            OnActivate?.Invoke();
+        }
+
+        //Деактивация баффа
+        public virtual void Deactivate() 
+        {
+            _baffStrength = 0;
+            _activeTime = 0;
+            OnDeactivate?.Invoke();
+        }
+        
+        void StartTimer(double time)
+        {
+            //Здесь необходима реализация кода таймера
+            //Есть вариант использовать карутины
+
+            if (time <= 0) 
+            {
+                onTimerEnd();
+            }
+        }
+
+        //по окончанию таймера выполняется деактивация
+        void onTimerEnd() 
+        {
+            Deactivate();
+        }
+
+        public bool IsActive => _activeTime > 0;
+
+        //события активации и деактивации бафа
+        public event BaffStatusChanged OnActivate;
+        public event BaffStatusChanged OnDeactivate;
+        
+    }
+}
---
+++ Health/HealthBaff.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace HealthControlling
+{
+    class HealthBaff: Baff
+    {
+        //Возможны доработки
+        private Health _health;
+        private int _oldMax;
+        private double _valueUpdateCoef;
+
+        public HealthBaff(Health health,string name, int strength, double time) : base(name,strength,time)
+        {
+            _health = health;
+        }
+
+        //В данном случае бафф временно увеличивает/уменьшает максимальное количество здоровья в зависимости от показателя силы баффа,
+        //а так же пропорциионально изменению максимального здоровья увеличинивает/уменьшает его значение 
+        public override void Activate()
+        {
+            _oldMax = _health.Max;
+            _health.MaxAdd(BaffStrength);
+            _valueUpdateCoef = _health.Max / _oldMax;
+            _health.Value=(int)_valueUpdateCoef*_health.Value;
+
+            base.Activate();
+        }
+
+        public override void Deactivate()
+        {
+            //Возвращается старое маскимальное значение хп и так же вновь пропорционально уменьшенное/увеличенное значение value
+            _health.Max = _oldMax;
+            _health.Value = (int)(1/_valueUpdateCoef) * _health.Value;
+
+
+            base.Deactivate();
+        }
+    }
+}
