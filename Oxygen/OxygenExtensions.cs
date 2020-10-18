25a30f2898e54d4f966fcdd25b5f24d34d8b00da
Domik0 <dafedot1@gmail.com>
2020-10-18 14:34:43 +0300

    Add Oxygen

---
+++ Oxygen/OxygenExtensions.cs
@@ -0,0 +1,53 @@
+﻿namespace OxygenControlling
+{
+    public static class OxygenExtension
+    {
+        public static (int, int) ValueSet(this Oxygen oxygen, int value)
+        {
+            var current = oxygen.Value;
+            oxygen.Value = value;
+            var diff = current - oxygen.Value;
+            return (oxygen.Value, diff);
+        }
+
+        public static (int, int) ValueAdd(this Oxygen oxygen, int value)
+        {
+            var current = oxygen.Value;
+            oxygen.Value += value;
+            var diff = oxygen.Value - current;
+            return (oxygen.Value, diff);
+        }
+
+        public static (int, int) ValueRemove(this Oxygen oxygen, int value)
+        {
+            var current = oxygen.Value;
+            oxygen.Value -= value;
+            var diff = current - oxygen.Value;
+            return (oxygen.Value, diff);
+        }
+
+        public static (int, int) MaxSet(this Oxygen oxygen, int value)
+        {
+            var current = oxygen.Max;
+            oxygen.Max = value;
+            var diff = oxygen.Max - current;
+            return (oxygen.Max, diff);
+        }
+
+        public static (int, int) MaxAdd(this Oxygen oxygen, int value)
+        {
+            var current = oxygen.Max;
+            oxygen.Max += value;
+            var diff = oxygen.Max - current;
+            return (oxygen.Max, diff);
+        }
+
+        public static (int, int) MaxRemove(this Oxygen oxygen, int value)
+        {
+            var current = oxygen.Max;
+            oxygen.Max -= value;
+            var diff = oxygen.Max - current;
+            return (oxygen.Max, diff);
+        }
+    }
+}
