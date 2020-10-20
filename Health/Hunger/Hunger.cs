e54b2bef7da2143c0cb9efa05b6ee5204744b815
ZVERZVE-NU16EJO\Zver <eemil02@mail.ru>
2020-10-20 09:43:07 +0300

    AddHunger

---
+++ Health/Hunger/Hunger.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HealthControlling
+{
+   public sealed class Hunger
+    {
+        public delegate void ChangedDelegate(int health, int value); 
+
+        private int _max; 
+        private int _value;
+
+        public Hunger(int max)
+        {
+            Max = max;
+        }
+
+        public int Max
+        {
+            get => _max; 
+            set
+            {
+                if (value == _max)
+                    return;
+
+                var oldMax = _max;
+                _max = value;
+                var maxDiff = _max - oldMax;
+               
+                if (_value > _max)
+                {
+                    var oldValue = _value;
+                    _value = _max;
+                    var diff = _value - oldValue;
+                    ValueChangedEvent?.Invoke(_value, diff);
+                }
+              
+
+                MaxChangedEvent?.Invoke(_max, maxDiff); 
+            }
+        }
+
+        public int Value
+        {
+            get => _value;
+            set
+            {
+                if (value == _value) 
+                    return;
+
+                var oldValue = _value;
+                _value = value > Max ? Max : value;
+                if (_value < 0) 
+                    _value = 0;
+                var diff = _value - oldValue;
+                ValueChangedEvent?.Invoke(_value, diff);
+          
+            
+                
+            }
+        }
+
+      
+        
+        public event ChangedDelegate ValueChangedEvent;
+        public event ChangedDelegate MaxChangedEvent;
+        
+
+         
+        public static implicit operator int(Hunger hunger) => hunger.Value;
+
+        public static Hunger operator +(Hunger hunger, int value) 
+        {
+            hunger.ValueAdd(value);
+            return hunger;
+        }
+
+        public static Hunger operator -(Hunger hunger, int value) 
+        {
+            hunger.ValueRemove(value);
+            return hunger;
+        }
+    }
+}
---
+++ Health/Hunger/HungerExtenshions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HealthControlling
+{
+   public static class HungerExtenshions
+    {
+        public static (int, int) ValueSet(this Hunger hunger, int value)
+        {
+            var current = hunger.Value;
+            hunger.Value = value;
+            var diff = current - hunger.Value;
+            return (hunger.Value, diff);
+        }
+
+        public static (int, int) ValueAdd(this Hunger hunger, int value)
+        {
+            var current = hunger.Value;
+            hunger.Value += value;
+            var diff = hunger.Value - current;
+            return (hunger.Value, diff);
+        }
+
+        public static (int, int) ValueRemove(this Hunger hunger, int value)
+        {
+            var current = hunger.Value;
+            hunger.Value -= value;
+            var diff = current - hunger.Value;
+            return (hunger.Value, diff);
+        }
+
+        public static (int, int) MaxSet(this Hunger hunger, int value)
+        {
+            var current = hunger.Max;
+            hunger.Max = value;
+            var diff = hunger.Max - current;
+            return (hunger.Max, diff);
+        }
+
+        public static (int, int) MaxAdd(this Hunger hunger, int value)
+        {
+            var current = hunger.Max;
+            hunger.Max += value;
+            var diff = hunger.Max - current;
+            return (hunger.Max, diff);
+        }
+
+        public static (int, int) MaxRemove(this Hunger hunger, int value)
+        {
+            var current = hunger.Max;
+            hunger.Max -= value;
+            var diff = hunger.Max - current;
+            return (hunger.Max, diff);
+        }
+    }
+}
