9a12408743f6b83cfa9f0124e74fc3978bddebd3
Domik0 <dafedot1@gmail.com>
2020-10-25 23:19:32 +0300

    The unfinished part of the "Air Starvation" effect

---
+++ Health/EffectHealthOxygen.cs
@@ -0,0 +1,61 @@
+﻿using HealthControlling;
+using OxygenControlling;
+using System;
+using System.Data;
+using System.Threading;
+
+namespace EffectHealthOxygenControlling
+{
+    public sealed class EffectHealthOxygen
+    {
+        //Вообще я не считаю это за код, фигня какая-та
+
+        /*
+         * Идея в том что есть функция которая вызывается при каждом изменении и в зависимости от нахождении героя 
+         * или других действий происходит изменение процентов эффектов
+         * 
+         * Надо будет потом еще доделать, что бы эффект утихал
+        */
+
+        public delegate void BaffStatus(Oxygen oxygen, Health health);
+
+        private double percentEffectHealth = 0;
+        private double percentEffectOxygen = 0;
+
+        private Timer Timer;
+
+        public void EffectOxygenHunger(Oxygen oxygen, Health health, bool flag)
+        {
+            ChangeProcentDamageHealth(oxygen, health);
+            ChangeProcentDamageOxygen(flag);
+            oxygen += (int)(oxygen.Max * percentEffectOxygen);
+            health += (int)(health * percentEffectHealth);
+        }
+
+        public void ChangeProcentDamageHealth(Oxygen oxygen, Health health)
+        {
+            if (oxygen < oxygen.Max * 0.2 && oxygen != 0)
+            {
+                percentEffectHealth = 0;
+            }
+            else if (oxygen == 0)
+            {
+                percentEffectHealth = -0.05;
+            }
+        }
+
+        public void ChangeProcentDamageOxygen(bool flag)
+        {
+            if (flag)
+            {
+                percentEffectOxygen = -0.1;
+            }
+            else
+            {
+                percentEffectOxygen = 0.1;
+            }
+        }
+
+        public event BaffStatus OxygenHunger;
+    }
+}
---
+++ Health/Oxygen.cs
@@ -0,0 +1,76 @@
+﻿using System;
+ 
+namespace OxygenControlling
+{
+    public sealed class Oxygen
+    {
+        public delegate void ChangedDelegate(int Oxygen, int value);
+
+        private int _max;
+        private int _value;
+        
+        public Oxygen(int max)
+        {
+            Max = max;
+        }
+
+        public int Max 
+        {
+            get => _max; 
+            set
+            {
+                if (value == _max)
+                    return;
+
+                var oldMax = _max;
+                _max = value;
+                var maxDiff = _max - oldMax;
+
+                if (_value > _max)
+                {
+                    var oldValue = _value;
+                    _value = _max;
+                    var diff = _value - oldValue;
+                    ValueChangedEvent?.Invoke(_value, diff);
+                }
+
+                MaxChangedEvent?.Invoke(_max, maxDiff);
+            }
+        }
+
+        public int Value
+        {
+            get => _value;
+            set
+            {
+                if (value == _value)
+                    return;
+
+                var oldValue = _value;
+                _value = value > Max ? Max : value;
+                if (_value < 0)
+                    _value = 0;
+                var diff = _value - oldValue;
+
+                ValueChangedEvent?.Invoke(_value, diff);
+            }
+        }
+
+        public event ChangedDelegate ValueChangedEvent;
+        public event ChangedDelegate MaxChangedEvent;
+
+        public static implicit operator int(Oxygen oxygen) => oxygen.Value;
+
+        public static Oxygen operator +(Oxygen oxygen, int value)
+        {
+            oxygen.ValueAdd(value);
+            return oxygen;
+        }
+
+        public static Oxygen operator -(Oxygen oxygen, int value)
+        {
+            oxygen.ValueRemove(value);
+            return oxygen;
+        }
+    }
+}
