a8bfa7ea47743522f268d979f2409d61be7352c9
ZVERZVE-NU16EJO\Zver <eemil02@mail.ru>
2020-10-29 20:46:34 +0300

    Add Update Method

--- Health/HungerEffect.cs
+++ Health/HungerEffect.cs
@@ -14,24 +14,65 @@ namespace HealthControlling.Hunger
 
         private readonly Hunger _hungry;
         private readonly Health _health;
+        private DateTime _lastLowTime;
         public TimeSpan Duration { get; set; } = TimeSpan.FromSeconds(2);
-        public Hunger Hungry => _hungry;
-        public Health Health => _health;
-
+        public HungerEffect(Hunger hungry, Health health)
+        {
+            _hungry = hungry;
+            _health = health;
+        }
 
         public void Update(DateTime current, HungryCondition condition)
         {
-            if (Health <= 0)
+            if (_health <= 0)
                 return;
 
             UpdateHungry(current, condition);
+
         }
 
         private void UpdateHungry(DateTime current, HungryCondition condition)
         {
-          
+            double persents = 1;
+            if (condition == HungryCondition.Hungry)
+            {
+                persents = -0.1;
+                _lastLowTime = current;
+            }
+            else
+            {
+                var delta = current - _lastLowTime;
+                if (delta <= Duration)
+                {
+                    persents = -0.05;
+                }
+            }
+
+            var value = (int)(_hungry.Max * persents);
+            _hungry.ValueAdd(value);
+
         }
+        private void UpdateHealth()
+        {
+            double percents = 0.0;
 
+            if (_hungry <= 0)
+                percents = -0.05;
+            else
+            {
+                var limit = _health.Max * 0.2;
+                if (_health < limit)
+                    percents = -0.01;
+                else if (limit < _hungry.Max)
+                    percents = 0.05;
+                else
+                    return;
+            }
 
+            var value = (int)(_health.Value * percents);
+            _health.ValueAdd(value);
+        }
     }
+
 }
+
